862826d98ca43dcf8776c537e4b71dadf181c9e2
leonardo-agueci <leoagueci@gmail.com>
2025-02-09 17:11:09 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# true-neutral-cookiecutter
-A minimalist cookiecutter for research and data science projects
+# Minimal Cookiecutter
+A minimalist cookiecutter for new research projects.
+It is a template for cookiecutter, a tool for quickly generate template-based new repositories.
+In order to use it, you'll need to first install cookiecutter. How:
 
-Run via `cookiecutter gh:patrickmineault/true-neutral-cookiecutter`
+```bash
+# Uncomment this part if you don't have pipx installed
+# sudo apt update
+# sudo apt install pipx
+# pipx ensurepath
+
+# Install cookiecutter
+pipx install cookiecutter
+```
+
+The repo was tested on linux, with uv package and project manager. Both uv and 
+cookiecutter should work on Windows and MacOs, but I didn't test it there yet.
+
+Run via `cookiecutter gh:leonardo-agueci/minimal-cookiecutter`
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,5 @@
+import time
+
 help = """
 Your project has been created!
 _____________________________________________________________________________
@@ -22,8 +24,21 @@ _____________________________________________________________________________
     :::::::  '''''''''''   ''''''''''''':::. -'\  \       C. SWANSIGER
 _____':::::_____________________________________\__\_________________________
 
-If you have not done so already, create a conda environment for your new 
-project with:
+"""
+instructions = """
+If you have not done so already, create a python virtual environment for your new 
+project:
+
+- Using uv (recommended)
+# Run if you don't have uv installed
+pip install uv
+# Create the environment (no ne)
+cd {{cookiecutter.repo_name}}
+uv venv create --python 3.10
+# Install dependencies
+uv install
+
+- Using conda
 
 cd {{cookiecutter.repo_name}}
 conda create --name {{cookiecutter.repo_name}} python=3.11
@@ -34,9 +49,13 @@ Install your new project in your local conda environment with:
 
 pip install -e .
 
-You will need to manually add data to .gitignore to prevent it from syncing to
-version control.
+IMPORTANT: You will need to manually add the environment folder (.venv if you 
+used the default in uv) to .gitignore to prevent it from syncing to version 
+control.
 
 Don't forget to sync to GitHub. Have fun!
 """
 print(help)
+time.sleep(3)
+print(instructions)
+
